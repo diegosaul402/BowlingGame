fe030be6f91f57704b7ce3af8dd18218634d91a6
Diego Mendoza <diego_saul_402@hotmail.com>
2018-03-27 12:51:26 -0600

    solve kata with proposed input

--- spec/bowling_kata_spec.rb
+++ spec/bowling_kata_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe BowlingKata do
-  it "test should run correctly" do
-    expect(true).to eq true
+  let(:game)    {BowlingKata.new}
+  let(:test_rolls) {[1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6]}
+  it "should return the right result" do
+    test_rolls.each {|roll| game.roll(roll)}
+
+    expect(game.score).to eq 133
+    byebug
   end
+
 end
