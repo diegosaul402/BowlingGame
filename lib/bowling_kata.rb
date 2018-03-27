3baee58b72a37cb4c26f55fc18bf2f95ebd0a7e0
Diego Mendoza <diego_saul_402@hotmail.com>
2018-03-27 16:18:25 -0600

    reduce the instance variables use

--- lib/bowling_kata.rb
+++ lib/bowling_kata.rb
@@ -1,6 +1,5 @@
 class BowlingKata
 @rolls
-@roll_index
 
   def initialize
     @rolls = Array.new
@@ -13,34 +12,34 @@ class BowlingKata
   def score
     current_frame = 0
     score = 0
-    @roll_index = 0
+    roll_index = 0
     while current_frame < 10 do
-      if is_strike
-        score += 10 + @rolls[@roll_index + 1] + @rolls[@roll_index + 2]
-        @roll_index += 1
+      if is_strike(roll_index)
+        score += 10 + @rolls[roll_index + 1] + @rolls[roll_index + 2]
+        roll_index += 1
         current_frame += 1
-      elsif is_spare
-        score += 10 + @rolls[@roll_index + 2]
-        @roll_index += 2
+      elsif is_spare(roll_index)
+        score += 10 + @rolls[roll_index + 2]
+        roll_index += 2
         current_frame += 1
-      elsif is_open
-        score += @rolls[@roll_index] + @rolls[@roll_index + 1]
-        @roll_index += 2
+      elsif is_open(roll_index)
+        score += @rolls[roll_index] + @rolls[roll_index + 1]
+        roll_index += 2
         current_frame += 1
       end
     end
     score
   end
 
-  def is_strike
-    @rolls[@roll_index] == 10
+  def is_strike(roll_index)
+    @rolls[roll_index] == 10
   end
 
-  def is_spare
-    @rolls[@roll_index] + @rolls[@roll_index + 1] == 10
+  def is_spare(roll_index)
+    @rolls[roll_index] + @rolls[roll_index + 1] == 10
   end
 
-  def is_open
-    @rolls[@roll_index] + @rolls[@roll_index + 1] != 10
+  def is_open(roll_index)
+    @rolls[roll_index] + @rolls[roll_index + 1] != 10
   end
 end
